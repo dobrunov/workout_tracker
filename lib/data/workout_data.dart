3592a3058c269417d8fe5187411ef75bd7df9511
dobrunov1 <e.dobrunov@gmail.com>
2023-02-09 17:14:14 +0200

    workout data metods complete

--- lib/data/workout_data.dart
+++ lib/data/workout_data.dart
@@ -24,12 +24,64 @@ class WorkoutData {
   ];
 
   // get the list of workouts
-
-  // add a workout
-
-  // add an exercise to a workout
-
-  // check off exercise
+  List<Workout> getWorkoutList() {
+    return workoutList;
+  }
 
   // get length of a given workout
+  int numberOfExercisesInWorkout(String workoutName) {
+    Workout relevantWorkout = getRelevantWorkout(workoutName);
+
+    return relevantWorkout.exercises.length;
+  }
+
+  // add a workout
+  void addWorkout(String name) {
+    // add a new workout with a blank list of exerises
+    workoutList.add(Workout(name: name, exercises: []));
+  }
+
+  // add an exercise to a workout
+  void addExercise(
+    String workoutName,
+    String exerciseName,
+    String weight,
+    String reps,
+    String sets,
+  ) {
+    // find the relevant workout
+    Workout relevantWorkout = getRelevantWorkout(workoutName);
+
+    relevantWorkout.exercises.add(Exercise(
+      name: exerciseName,
+      weight: weight,
+      reps: reps,
+      sets: sets,
+    ));
+  }
+
+  // check off exercise
+  void checkOffExercise(String workoutName, String exerciseName) {
+    // find the relevant workout and relevant exercise in that workout
+    Exercise relevantExercise = getRelevantExercise(workoutName, exerciseName);
+
+    // check off boolean to show user completed the exercise
+    relevantExercise.isCompleted = !relevantExercise.isCompleted;
+  }
+
+  // return relevant workout object, given a workout name
+  Workout getRelevantWorkout(String workoutName) {
+    Workout relevantWorkout = workoutList.firstWhere((workout) => workout.name == workoutName);
+    return relevantWorkout;
+  }
+
+  // return relevant workout object, given a workout name + exercise name
+  Exercise getRelevantExercise(String workoutName, String exerciseName) {
+    // find the relevant workout
+    Workout relevantWorkout = getRelevantWorkout(workoutName);
+
+    // then find the relevant exercise in that workout
+    Exercise relevantExercise = relevantWorkout.exercises.firstWhere((exercise) => exercise.name == exerciseName);
+    return relevantExercise;
+  }
 }
